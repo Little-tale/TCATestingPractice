a93751a060ff3fdd9f2a3f39d18938da64db79ff
Kim JaeHyung <you3192001@icloud.com>
2024-07-30 11:44:28 +0900

    Initial Commit

---
+++ TCATestingPractice/TCATestingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATestingPracticeApp.swift
+//  TCATestingPractice
+//
+//  Created by Jae hyung Kim on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TCATestingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
