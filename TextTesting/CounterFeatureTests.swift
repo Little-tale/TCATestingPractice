45d320d0bbb424f974d1a05372b87a49e286c69f
Kim JaeHyung <you3192001@icloud.com>
2024-08-07 15:24:23 +0900

    Fetch: 카운터 피처 + 테스팅 생성

---
+++ TextTesting/CounterFeatureTests.swift
@@ -0,0 +1,21 @@
+//
+//  CounterFeatureTests.swift
+//  TextTesting
+//
+//  Created by Jae hyung Kim on 8/7/24.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import TCATestingPractice
+
+
+final class CounterFeatureTests: XCTestCase {
+    
+    @MainActor
+    func testCounter() async {
+        
+    }
+    
+}
